405ef2315be55136c2a7c869e393f0e904f026d9
Aycan Sapmaz <sapmaz72@gmail.com>
2026-01-12 21:14:28 +0100

    feat: update document title on route change

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,11 @@
+import Seo from "../components/Seo";
+
 export default function Home() {
-    return <h1>Home</h1>;
+    return (
+        <>
+            <Seo title="Home" description="Home Page" />
+            <h1>Home</h1>
+        </>
+    )
+        ;
 }
